d3e1b73fad940bc79bc450f66c4a7a174d5096ae
Android40 <vishalyadav.think360@gmail.com>
2018-07-02 16:49:36 +0530

    Removed URL

--- app/src/main/java/responsehandling/app/com/refrecdemo/retrofit/ApiClient.java
+++ app/src/main/java/responsehandling/app/com/refrecdemo/retrofit/ApiClient.java
@@ -7,7 +7,6 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static String BASE_URL = "http://push.multicast-media.com/backend/v1/";
     private static Retrofit retrofit = null;
 
 
@@ -19,7 +18,7 @@ public class ApiClient {
                 .build();
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl("BASE_URL_HERE")
                     .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
